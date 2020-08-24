10a9cba4dd1041cc94dbba117b5707ba2f282a45
selam lemma <oneiconforlife26@gmail.com>
2020-08-24 07:15:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 def speak_to_grandma(phrase)
-  if phrase != phrase.uppercase
+  if phrase != phrase.upcase
      'HUH?! SPEAK UP, SONNY!'
+  elsif phrase == I LOVE YOU GRANDMA!
+      'I LOVE YOU TOO PUMPKIN!'
    end
 end
 
